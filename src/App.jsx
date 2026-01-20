a9b1d50f480b04938ec5f3cb8c627e5fb4a4cfb1
Denisio Rodrigues <denisio@ymail.com>
2026-01-20 20:35:50 -0300

    Formulario de eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
 import './App.css'
 
+function TituloFormulario(props) {
+  return (
+    <h2>{props.children}</h2>
+  )
+}
+
+function CampoDeFormulario({children}) {
+  return (
+    <fieldset>
+      {children}
+    </fieldset>
+  )
+}
+
+function Label({children, htmlFor}) {
+  return (
+    <label htmlFor={htmlFor}>
+      {children}
+    </label>
+  )
+}
+
+function CampoDeEntrada(props) {
+  return (
+    <input {...props} />
+  )
+}
+
+function FormularioDeEvento() {
+  return (
+    <form className='form-evento' action="">
+      <TituloFormulario>Preencha para criar um evento</TituloFormulario>
+      <CampoDeFormulario>
+        <Label htmlFor="nome">Qual é o nome do evento?</Label>
+        <CampoDeEntrada type="text" id='nome' placeholder='Summer dev hits' />
+      </CampoDeFormulario>
+    </form>
+  )
+}
+
 function App() {
 
   return (
@@ -11,6 +51,8 @@ function App() {
         <section className='banner'>
           <img src="/banner.png" alt="banner de uma moca com fones de ouvido" />
         </section>
+
+        <FormularioDeEvento />
       </main>
     </>
   )
