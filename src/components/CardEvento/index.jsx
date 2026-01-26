9d9a830c1a9973d48e19e0d2ad2a83128ad9c2b1
Denisio Rodrigues <denisio@ymail.com>
2026-01-26 09:29:17 -0300

    card de evento - ajuste na data

--- src/components/CardEvento/index.jsx
+++ src/components/CardEvento/index.jsx
@@ -6,7 +6,7 @@ export function CardEvento({evento}) {
         <img src={evento.capa} alt={evento.titulo} />
         <div className="card-evento corpo">
           <p className="card-evento tag"> {evento.tema.nome}</p>
-          <p className="card-evento data">{evento.data.toLocaleString("pt-BR")}</p>
+          <p className="card-evento data">{evento.data.toLocaleDateString("pt-BR")}</p>
           <h4 className="card-evento titulo">{evento.titulo}</h4>
         </div>
       </div>
